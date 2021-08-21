e04b7120a9e391417606e8b89759fced64dde7ac
Joseph Schittina <joseph.schittina@uconn.edu>
2021-08-20 22:21:22 -0400

    updated format
    Clock/resume/bio/intro complete

--- main.js
+++ main.js
@@ -1,35 +1,4 @@
 
-document.getElementById("dark").onclick=function(){
-  document.getElementById("topp").style.backgroundColor='rgb(0,0,0)';
-  document.getElementById("nam").style.color='rgb(145,145,145)';
-  document.getElementById("scho").style.color='rgb(145,145,145)';
-  document.getElementById("maj").style.color='rgb(145,145,145)';
-  document.getElementById("resu").style.color='rgb(145,145,145)';
-  document.getElementById("gall").style.color='rgb(145,145,145)';
-  document.getElementById("link").style.color='rgb(145,145,145)';
-  document.getElementById("funp").style.color='rgb(145,145,145)';
-  document.getElementById("textbox").style.backgroundColor='rgba(0,0,0,.35)';
-  document.getElementById("textbox").style.color='rgba(255,255,255,.5)';
-  document.getElementById("clock").style.backgroundColor='rgba(0,0,0,.35)';
-  document.getElementById("clock").style.color="rgb(30, 230, 88)";
-}
-
-document.getElementById("light").onclick=function(){
-  document.getElementById("topp").style.backgroundColor='white';
-  document.getElementById("nam").style.color='black';
-  document.getElementById("scho").style.color='black';
-  document.getElementById("maj").style.color='black';
-  document.getElementById("resu").style.color='black';
-  document.getElementById("gall").style.color='black';
-  document.getElementById("link").style.color='black';
-  document.getElementById("funp").style.color='black';
-  document.getElementById("textbox").style.backgroundColor='rgba(250,250,250,.25)';
-  document.getElementById("textbox").style.color='black';
-  document.getElementById("clock").style.backgroundColor='rgba(250,250,250,.25)';
-  document.getElementById("clock").style.color='rgb(59, 230, 88)';
-
-}
-
 function currentTime() {
 var date = new Date(); 
 var hour = date.getHours();
@@ -56,11 +25,4 @@ else {
 
 currentTime();
 
-document.getElementById("finbut").onclick=function(){
-document.getElementById("finbut").innerHTML='<img class="finny" src="IMG_3163.jpg" alt="finny picture">';
-}
-
-document.getElementById("magbut").onclick=function(){
-document.getElementById("magbut").innerHTML='<img class="maggie" src="IMG_6691.jpg" alt="maggie picture">';
-}
 
