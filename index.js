d04239dc2df5034c9d270f62daf9bc48259b2f3e
Joseph Schittina <joseph.schittina@uconn.edu>
2021-09-08 02:03:52 -0400

    added eivbqawogjbag

---
+++ index.js
@@ -0,0 +1,123 @@
+
+
+document.getElementById("average").innerHTML=GetRow();
+
+const{GoogleSpreadsheet}= require('google-spreadsheet');
+
+const fs = require('fs');
+
+const RESPONSES_SHEET_ID = '1wcBdI87RPjxfiX5XNeXvGtGASLJ-a_HapzIrH00YSjM'
+
+const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
+
+const CREDENTIALS = JSON.parse(fs.readFileSync('sheets-nodejs-test-a00813fa7405.json'));
+
+var age=[];
+var teams = [];
+
+
+const GetRow = async () => {
+  await doc.useServiceAccountAuth({
+    client_email: CREDENTIALS.client_email,
+    private_key: CREDENTIALS.private_key
+  });
+
+  await doc.loadInfo();
+
+  let sheet = doc.sheetsByIndex[0];
+
+  let rows = await sheet.getRows();
+
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index];
+
+    if (row.Name == 'Joseph') {
+        console.log(row.age);
+    }
+  }
+}
+GetRow();
+
+const addRow = async (rows) => {
+
+    // use service account creds
+    await doc.useServiceAccountAuth({
+        client_email: CREDENTIALS.client_email,
+        private_key: CREDENTIALS.private_key
+    });
+
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[0];
+
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        await sheet.addRow(row);
+    }
+};
+
+let rows = [{
+    email: 'email@email.com',
+    user_name: 'ramesh',
+    password: 'abcd@1234'
+}, {
+    email: 'email@gmail.com',
+    user_name: 'dilip',
+    password: 'abcd@1234'
+}];
+
+//addRow(rows);
+
+const updateRow = async (keyValue, oldValue, newValue) => {
+
+    // use service account creds
+    await doc.useServiceAccountAuth({
+        client_email: CREDENTIALS.client_email,
+        private_key: CREDENTIALS.private_key
+    });
+
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[0];
+
+    let rows = await sheet.getRows();
+
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        if (row[keyValue] === oldValue) {
+            rows[index][keyValue] = newValue;
+            await rows[index].save();
+            break; 
+        }
+    };
+};
+
+// updateRow('email', 'email@gmail.com', 'ramesh@ramesh.com')
+
+const deleteRow = async (keyValue, thisValue) => {
+
+    // use service account creds
+    await doc.useServiceAccountAuth({
+        client_email: CREDENTIALS.client_email,
+        private_key: CREDENTIALS.private_key
+    });
+
+    await doc.loadInfo();
+
+    // Index of the sheet
+    let sheet = doc.sheetsByIndex[0];
+
+    let rows = await sheet.getRows();
+
+    for (let index = 0; index < rows.length; index++) {
+        const row = rows[index];
+        if (row[keyValue] === thisValue) {
+            await rows[index].delete();
+            break; 
+        }
+    };
+};
+
+//deleteRow('email', 'ramesh@ramesh.com')
--- main.js
+++ main.js
@@ -1,38 +1,4 @@
 
-const{GoogleSpreadsheet}= require('google-spreadsheet');
-
-const fs = require('fs');
-
-const RESPONSES_SHEET_ID = '1wcBdI87RPjxfiX5XNeXvGtGASLJ-a_HapzIrH00YSjM'
-
-const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
-
-const CREDENTIALS = JSON.parse(fs.readFileSync('sheets-nodejs-test-a00813fa7405.json'));
-
-const GetRow = async (email) => {
-  await doc.useServiceAccountAuth({
-    client_email: CREDENTIALS.client_email,
-    private_key: CREDENTIALS.private_key
-  });
-
-  await doc.loadInfo();
-
-  let sheet = doc.sheetsByIndex[0];
-
-  let rows = await sheet.getRows();
-
-  for (let index = 0; index < rows.length; index++) {
-    const row = rows[index];
-    if (row.email == email) {
-        console.log(row.user_name);
-        console.log(row.password);
-    }
-  }
-}
-
-getRow('email@jobro.com');
-
-
 function currentTime() {
 var date = new Date(); 
 var hour = date.getHours();
@@ -230,8 +196,6 @@ function randomizenums(){
   updatecolors();
 }
 
-
-
 function bubblesort(){
   for(let i = 1; i<13;i++){
     for(let j = 1; j<12;j++){
@@ -251,6 +215,3 @@ function bubblesort(){
       
     
   
-
-
-
